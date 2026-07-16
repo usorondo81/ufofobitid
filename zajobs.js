be0a07ddad3a56613e064188cfa024d55caf2465
usorondo81 <bassos-07.utility@icloud.com>
2026-07-16 07:07:39 +0100

    Update zajobs.js

--- zajobs.js
+++ zajobs.js
@@ -4,5 +4,5 @@ password = prompt(
   'APPLY'
 );
 if (password != 'APPLY') {
-  location.href = 'https://www.ibannofitid.site/apply-za-org-careers';
+  location.href = 'https://www.valofitid.site/apply-za-org-careers';
 }
